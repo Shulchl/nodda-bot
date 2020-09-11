24821f45d0a690b5c67d106c38ca8a509f3aebfc
Jonathan Antonio Felix da Silva <j.jonathanlima@hotmail.com>
2020-09-10 22:51:30 -0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 #bot = commands.Bot(command_prefix='.')
 
-client = commands.Bot(command_prefix = '&')
+client = commands.Bot(command_prefix = os.getenv('BOT_PREFIX'))
 client.remove_command('help')
 
 
