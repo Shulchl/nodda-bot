f7ecc5c016284b328e0bc2bdad9ba40d40d4fa30
Jonathan Felix <j.jonathanlima@hotmail.com>
2020-10-15 18:57:38 -0300

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -64,7 +64,7 @@ class Moderation(commands.Cog, name='Moderação'):
         #"""Gives them inferno."""
         await mute(ctx, user, reason or "Desrespeito às regras.") # uses the mute function
         channel = discord.utils.get(ctx.guild.text_channels, name="inferno")
-        await channel.send(f'Olá, {user}! Seja bem vindo ao {channel}.\n Você foi exilado para cá até que seja desmutado. *Aprecie o silêncio*.')
+        await channel.send(f'Olá, {user.mention}! Seja bem vindo ao {channel}.\n Você foi exilado para cá até que seja desmutado. *Aprecie o silêncio*.')
 
     @commands.command(name='unmute', help='Desmuta um usuário ao digitar `%unmute <usuário>`')
     async def unmute(self, ctx, user: Redeemed):
