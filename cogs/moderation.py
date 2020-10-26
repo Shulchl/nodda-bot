8340720281637802e0264f18b5ecf8b6b746fd96
Jonathan Felix <j.jonathanlima@hotmail.com>
2020-10-26 14:34:43 -0300

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -82,12 +82,11 @@ class Moderation(commands.Cog, name='Moderação'):
     @commands.cooldown(1, 10, commands.BucketType.user)
     async def clear(self, ctx, amount : int):
         await ctx.channel.purge(limit=amount + 1)
-        emb = discord.Embed(title='Limpei!',description=f'{amount} mensagens foram apagadas!',color=discord.Color.black())
-        asyncio.sleep(5)
+        await asyncio.sleep(2)
+        emb = discord.Embed(title='Limpei!',description='{} mensagens foram apagadas!'.format(amount),color=discord.Color.red())
         await ctx.send('',embed=emb)
-        asyncio.sleep(1)
-        await ctx.channel.purge(limit=1)
-        
+        await asyncio.sleep(2)
+        await ctx.channel.purge(limit=1)  
     @clear.error
     async def clear_error(self, ctx, error):
         if isinstance(error, commands.UserInputError):
